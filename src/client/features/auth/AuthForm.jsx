2b44d0255fc099bf80f8ce795ca0cad22677b630
Lester Lee <contact@lester-lee.com>
2023-11-06 13:49:58 -0500

    Remove placeholder from authform'

--- src/client/features/auth/AuthForm.jsx
+++ src/client/features/auth/AuthForm.jsx
@@ -14,8 +14,8 @@ export default function AuthForm() {
     : "Already have an account? Login here.";
 
   // Controlled form fields
-  const [username, setUsername] = useState("foo");
-  const [password, setPassword] = useState("bar");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   // Form submission
   const [login, { isLoading: loginLoading, error: loginError }] =
