983d5dc1cab45854030cb8cbb725bbf3ecd7cb4b
Brian Capraro <Briancapraro0228@gmail.com>
2023-11-09 10:19:06 -0500

    Added code to Student.jsx for single student viewing. Will add update function with form later.

--- src/client/features/Students/Student.jsx
+++ src/client/features/Students/Student.jsx
@@ -1,7 +1,33 @@
-export default function Student ({student}) {
+import { useDeleteStudentMutation, useGetStudentQuery } from "./studentSlice";
+
+export default function Student() {
+    const {id} = useParams();
+    const { data, isLoading, isError } = useGetStudentQuery(id);
+    const [deleteStudent] = useDeleteStudentMutation();
+
+    const onDelete = async (evt) => {
+        evt.preventDefault();
+        deleteStudent(student.id);
+      };
+
+      if (isLoading) {
+        return <h1>Loading Student ...</h1>;
+      }
+    
+      if (isError || !data) {
+        return <h1>Error loading data</h1>;
+      }
+    
+      const student = data.student
+    
     return (
         <div>
-            <h1>{student.firstName}</h1>
+            <img src={student.imgUrl} />
+            <h2>{student.firstName} {student.lastName}</h2>
+            <p>Email: {student.email}</p>
+            <p>GPA: {student.gpa}</p>
+            <p>Campus:</p>
+            <button onClick={onDelete} aria-label="delete"></button>
         </div>
     )
 }
