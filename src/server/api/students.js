8a2ae6bc6935f0aed80bf08c1a5a7c3a87e48d17
Brian Capraro <Briancapraro0228@gmail.com>
2023-11-09 11:04:26 -0500

    Added create student route in students.js for backend.

--- src/server/api/students.js
+++ src/server/api/students.js
@@ -4,11 +4,30 @@ const router = require("express").Router();
 
 module.exports = router;
 
-// Sends all students 
+// Sends all students
 router.get("/", async (req, res, next) => {
   try {
     const students = await prisma.students.findMany();
     res.json(students);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Updates student and sends it
+router.post("/", async (req, res, next) => {
+  try {
+    const { firstName, lastName, email, gpa, imageUrl } = req.body;
+    const student = await prisma.students.create({
+      data: {
+        firstName,
+        lastName,
+        email,
+        imageUrl,
+        gpa,
+      },
+    });
+    res.json(student);
   } catch (err) {
     next(err);
   }
