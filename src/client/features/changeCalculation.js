f8f43862e370e1e71d92f001faa308a522f8c39b
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2023-11-28 14:12:35 -0500

    initial change rendering logic

--- src/client/features/changeCalculation.js
+++ src/client/features/changeCalculation.js
@@ -1,4 +1,4 @@
-const changeCalculated = {
+const calculatedChange = {
   twenties: 0,
   tens: 0,
   fives: 0,
@@ -10,84 +10,84 @@ const changeCalculated = {
 };
 
 function calculateTwenties(value) {
-  changeCalculated.twenties = Math.floor(value / 20);
+  calculatedChange.twenties = Math.floor(value / 20);
 }
 
 function calculateTens(value) {
-  changeCalculated.tens = Math.floor(value / 10);
+  calculatedChange.tens = Math.floor(value / 10);
 }
 
 function calculateFives(value) {
-  changeCalculated.fives = Math.floor(value / 5);
+  calculatedChange.fives = Math.floor(value / 5);
 }
 
 function calculateSingles(value) {
-  changeCalculated.singles = Math.floor(value / 1);
+  calculatedChange.singles = Math.floor(value / 1);
 }
 
 function calculateQuarters(value) {
-  changeCalculated.quarters = Math.floor(value / 0.25);
+  calculatedChange.quarters = Math.floor(value / 0.25);
 }
 
 function calculateDimes(value) {
-  changeCalculated.dimes = Math.floor(value / 0.1);
+  calculatedChange.dimes = Math.floor(value / 0.1);
 }
 
 function calculateNickels(value) {
-  changeCalculated.nickels = Math.floor(value / 0.05);
+  calculatedChange.nickels = Math.floor(value / 0.05);
 }
 
 function calculatePennies(value) {
-  changeCalculated.pennies = Math.floor(value / 0.01);
+  calculatedChange.pennies = Math.floor(value / 0.01);
 }
 
 function calculateChange(value) {
   if (value >= 20) {
     calculateTwenties(value);
-    value -= changeCalculated.twenties * 20;
+    value -= calculatedChange.twenties * 20;
     value = value.toFixed(2);
   }
 
   if (value >= 10) {
     calculateTens(value);
-    value -= changeCalculated.tens * 10;
+    value -= calculatedChange.tens * 10;
     value = value.toFixed(2);
   }
 
   if (value >= 5) {
     calculateFives(value);
-    value -= changeCalculated.fives * 5;
+    value -= calculatedChange.fives * 5;
     value = value.toFixed(2);
   }
 
   if (value >= 1) {
     calculateSingles(value);
-    value -= changeCalculated.singles * 1;
+    value -= calculatedChange.singles * 1;
     value = value.toFixed(2);
   }
 
   if (value >= 0.25) {
     calculateQuarters(value);
-    value -= changeCalculated.quarters * 0.25;
+    value -= calculatedChange.quarters * 0.25;
     value = value.toFixed(2);
   }
 
   if (value >= 0.1) {
     calculateDimes(value);
-    value -= changeCalculated.dimes * 0.1;
+    value -= calculatedChange.dimes * 0.1;
     value = value.toFixed(2);
   }
 
   if (value >= 0.05) {
     calculateNickels(value);
-    value -= changeCalculated.nickels * 0.05;
+    value -= calculatedChange.nickels * 0.05;
     value = value.toFixed(2);
   }
   if (value > 0) {
     calculatePennies(value);
-    value -= changeCalculated.pennies * 0.01;
+    value -= calculatedChange.pennies * 0.01;
   }
-  return changeCalculated;
+  return calculatedChange;
 }
 
 module.exports = calculateChange;
