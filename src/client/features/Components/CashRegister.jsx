ab8485915ac95d4e8795a48ba1f3de420c64f02f
Timothy Chang <123012347+Proton2050@users.noreply.github.com>
2023-11-29 13:05:27 -0800

    Add cart update

--- src/client/features/Components/CashRegister.jsx
+++ src/client/features/Components/CashRegister.jsx
@@ -7,18 +7,28 @@ const CashRegister = () => {
   const total = useSelector((state) => state.cart.totalPrice);
   console.log("total", total);
 
+  const cartItems = useSelector((state) => state.cart.cartItems);
+  console.log("cart items", cartItems);
+
   return isLoading ? (
     <h2>Loading...</h2>
   ) : (
     <div>
       <div className="product-container">
         <ul className="product-list">
-          {[...products].map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
+          {products.map((product) => {
+            <ProductCard key={product.id} product={product} />;
+          })}
         </ul>
       </div>
-      <div className="cart"></div>
+      <div className="cart">
+        <ul className="cart-list">
+          {cartItems.map((product) => {
+            <CartCard key={product.id} item={product} />;
+          })}
+        </ul>
+        <h2>Total: {total}</h2>
+      </div>
     </div>
   );
 };
--- src/client/features/Components/ProductCard.jsx
+++ src/client/features/Components/ProductCard.jsx
@@ -1,23 +1,18 @@
 import { useDispatch } from "react-redux";
-import { addToCart, removeFromCart } from "../Slices/cartSlice";
+import { addToCart } from "../Slices/cartSlice";
 
 const ProductCard = ({ product }) => {
-  const { id, name, price } = product;
+  const { name, imageUrl } = product;
   const dispatch = useDispatch();
 
   const handleAddToCart = (product) => {
     dispatch(addToCart(product));
   };
 
-  // Needs to be moved to CartCard
-  const handleRemoveFromCart = (product) => {
-    dispatch(removeFromCart(product));
-  };
-
   return (
     <li className="product" onClick={() => handleAddToCart(product)}>
+      <img src={imageUrl} />
       <h3>{name}</h3>
-      <p>{price}</p>
     </li>
   );
 };
