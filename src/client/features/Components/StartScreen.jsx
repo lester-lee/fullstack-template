8f2b01647f2a34725eb08d745b806ea46fd4cbd8
Marianne Anderson <mganderson13@gmail.com>
2023-11-29 17:30:03 -0600

    finished total bar

--- src/client/features/Components/StartScreen.jsx
+++ src/client/features/Components/StartScreen.jsx
@@ -1,12 +1,9 @@
 import { Link } from "react-router-dom";
-import Totalbar from "../../layout/Totals_Navbar";
+
 
 export default function StartScreen() {
    return (
    <>
-   <div>
-      <Totalbar />
-   </div>
    <div>
    <Link className="start-button" to={'/products'}> Start </Link>
    </div>
