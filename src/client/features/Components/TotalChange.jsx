9eaf581885658c984cf477a99c39f99a8a3ba28b
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2023-11-30 14:26:42 -0500

    added images to totalChange page - still need to sort of rendering the quantities of bills, and the map key error

--- src/client/features/Components/TotalChange.jsx
+++ src/client/features/Components/TotalChange.jsx
@@ -1,15 +1,17 @@
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import "../../../images/images.css";
+
 //dummydata
 // const calculatedChange = {
 //   twenties: 2,
 //   tens: 3,
 //   fives: 1,
-//   singles: 0,
-//   quarters: 2,
-//   dimes: 0,
-//   nickels: 4,
-//   pennies: 0,
+//   singles: 1,
+//   quarters: 1,
+//   dimes: 1,
+//   nickels: 1,
+//   pennies: 1,
 // };
 
 export default function TotalChange() {
@@ -38,7 +40,33 @@ export default function TotalChange() {
     <div>
       <h1>This will show the full amount of change</h1>
       {usableKeys.map((key) => (
-        <p key={key}>{key}</p>
+        <>
+          <p key={key}>{key}</p>
+          {key === "twenties" ? (
+            <img className="bills" src="src/images/twenty-dollar-bill.jpeg" />
+          ) : null}
+          {key === "tens" ? (
+            <img className="bills" src="src/images/ten-dollar-bill.jpg" />
+          ) : null}
+          {key === "fives" ? (
+            <img className="bills" src="src/images/five-dollar-bill.jpg" />
+          ) : null}
+          {key === "singles" ? (
+            <img className="bills" src="src/images/one-dollar-bill.jpg" />
+          ) : null}
+          {key === "quarters" ? (
+            <img className="coins quarter" src="src/images/quarter.jpeg" />
+          ) : null}
+          {key === "dimes" ? (
+            <img className="coins dime" src="src/images/dime.jpeg" />
+          ) : null}
+          {key === "nickels" ? (
+            <img className="coins nickel" src="src/images/nickel.jpeg" />
+          ) : null}
+          {key === "pennies" ? (
+            <img className="coins penny" src="src/images/penny.jpeg" />
+          ) : null}
+        </>
       ))}
       {valuesQueue.map((value) => (
         <p key={Math.random(100)}>{value}</p>
