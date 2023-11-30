d79bb69117550479eb9b45bbf53c54b1773e3aaf
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2023-11-30 11:58:26 -0500

    added conditional counter rendering for each bill

--- src/client/features/Components/ReceivedBills.jsx
+++ src/client/features/Components/ReceivedBills.jsx
@@ -10,10 +10,19 @@ import "../../../images/images.css";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { addTotalReceived } from "../slices/cartSlice";
+import { useState } from "react";
 
 export default function ReceivedBills() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  const [hundreds, setHundreds] = useState(0);
+  const [fifties, setFifties] = useState(0);
+  const [twenties, setTwenties] = useState(0);
+  const [tens, setTens] = useState(0);
+  const [fives, setFives] = useState(0);
+  const [ones, setOnes] = useState(0);
+
   return (
     <>
       <h1>Received Bills page</h1>
@@ -21,43 +30,67 @@ export default function ReceivedBills() {
         src="src/images/hundred-dollar-bill.jpeg"
         alt="hundred-dollar-bill"
         className="bills"
-        onClick={() => dispatch(addTotalReceived({ value: 100 }))}
+        onClick={() => {
+          dispatch(addTotalReceived({ value: 100 }));
+          setHundreds(hundreds + 1);
+        }}
       />
+      {hundreds > 0 ? <p>x {hundreds}</p> : null}
       <br />
       <img
         src="src/images/fifty-dollar-bill.jpeg"
         alt="fifty-dollar-bill"
         className="bills"
-        onClick={() => dispatch(addTotalReceived({ value: 50 }))}
+        onClick={() => {
+          dispatch(addTotalReceived({ value: 50 }));
+          setFifties(fifties + 1);
+        }}
       />
+      {fifties > 0 ? <p>x {fifties}</p> : null}
       <br />
       <img
         src="src/images/twenty-dollar-bill.jpeg"
         alt="twenty-dollar-bill"
         className="bills"
-        onClick={() => dispatch(addTotalReceived({ value: 20 }))}
+        onClick={() => {
+          dispatch(addTotalReceived({ value: 20 }));
+          setTwenties(twenties + 1);
+        }}
       />
+      {twenties > 0 ? <p>x {twenties}</p> : null}
       <br />
       <img
         src="src/images/ten-dollar-bill.jpg"
         alt="ten-dollar-bill"
         className="bills"
-        onClick={() => dispatch(addTotalReceived({ value: 10 }))}
+        onClick={() => {
+          dispatch(addTotalReceived({ value: 10 }));
+          setTens(tens + 1);
+        }}
       />
+      {tens > 0 ? <p>x {tens}</p> : null}
       <br />
       <img
         src="src/images/five-dollar-bill.jpg"
         alt="five-dollar-bill"
         className="bills"
-        onClick={() => dispatch(addTotalReceived({ value: 5 }))}
+        onClick={() => {
+          dispatch(addTotalReceived({ value: 5 }));
+          setFives(fives + 1);
+        }}
       />
+      {fives > 0 ? <p>x {fives}</p> : null}
       <br />
       <img
         src="src/images/one-dollar-bill.jpg"
         alt="one-dollar-bill"
         className="bills"
-        onClick={() => dispatch(addTotalReceived({ value: 1 }))}
+        onClick={() => {
+          dispatch(addTotalReceived({ value: 1 }));
+          setOnes(ones + 1);
+        }}
       />
+      {ones > 0 ? <p>x {ones}</p> : null}
       <br />
       <button onClick={() => navigate("/received-coins")}>Next</button>
     </>
