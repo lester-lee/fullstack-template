69dc31fbd17d6495c5c20d239001e2334b762a7a
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2023-11-30 10:55:36 -0500

    added images folder with all denominations, and added clickable bills to ReceivedBills.jsx

--- src/client/features/Components/ReceivedBills.jsx
+++ src/client/features/Components/ReceivedBills.jsx
@@ -5,6 +5,7 @@
 // a delete button
 
 // next button
+import "../../../images/images.css";
 
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
@@ -16,21 +17,47 @@ export default function ReceivedBills() {
   return (
     <>
       <h1>Received Bills page</h1>
-      <button onClick={() => dispatch(addTotalReceived({ value: 20 }))}>
-        20
-      </button>
+      <img
+        src="src/images/hundred-dollar-bill.jpeg"
+        alt="hundred-dollar-bill"
+        className="bills"
+        onClick={() => dispatch(addTotalReceived({ value: 100 }))}
+      />
       <br />
-      <button onClick={() => dispatch(addTotalReceived({ value: 10 }))}>
-        10
-      </button>
+      <img
+        src="src/images/fifty-dollar-bill.jpeg"
+        alt="fifty-dollar-bill"
+        className="bills"
+        onClick={() => dispatch(addTotalReceived({ value: 50 }))}
+      />
       <br />
-      <button onClick={() => dispatch(addTotalReceived({ value: 5 }))}>
-        5
-      </button>
+      <img
+        src="src/images/twenty-dollar-bill.jpeg"
+        alt="twenty-dollar-bill"
+        className="bills"
+        onClick={() => dispatch(addTotalReceived({ value: 20 }))}
+      />
       <br />
-      <button onClick={() => dispatch(addTotalReceived({ value: 1 }))}>
-        1
-      </button>
+      <img
+        src="src/images/ten-dollar-bill.jpg"
+        alt="ten-dollar-bill"
+        className="bills"
+        onClick={() => dispatch(addTotalReceived({ value: 10 }))}
+      />
+      <br />
+      <img
+        src="src/images/five-dollar-bill.jpg"
+        alt="five-dollar-bill"
+        className="bills"
+        onClick={() => dispatch(addTotalReceived({ value: 5 }))}
+      />
+      <br />
+      <img
+        src="src/images/one-dollar-bill.jpg"
+        alt="one-dollar-bill"
+        className="bills"
+        onClick={() => dispatch(addTotalReceived({ value: 1 }))}
+      />
       <br />
       <button onClick={() => navigate("/received-coins")}>Next</button>
     </>
