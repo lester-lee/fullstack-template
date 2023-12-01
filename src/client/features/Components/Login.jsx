57f267ff9afc919661eac14be1f3412523882caa
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2023-12-01 12:27:21 -0500

    functioning setEmail and setPassword

--- src/client/features/Components/Login.jsx
+++ src/client/features/Components/Login.jsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <>
       <h1>Login Page</h1>
       <form className="login-form">
-        <label>Email:</label>
-        <input type="email"></input>
+        <label className="form-labels">Email: </label>
+        <input
+          className="form-inputs"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
+        <label className="form-labels">Password: </label>
+        <input
+          className="form-inputs"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
       </form>
     </>
   );
