1bb0d7a6f993be8f4d7bc54fc577b30cc2c79948
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2023-12-04 11:14:45 -0500

    deleted ind functions and put definitions within if statements

--- src/client/features/changeCalculation.js
+++ src/client/features/changeCalculation.js
@@ -10,81 +10,49 @@ export default function calculateChange(value) {
     Pennies: 0,
   };
 
-  function calculateTwenties(value) {
-    calculatedChange.Twenties = Math.floor(value / 20);
-  }
-
-  function calculateTens(value) {
-    calculatedChange.Tens = Math.floor(value / 10);
-  }
-
-  function calculateFives(value) {
-    calculatedChange.Fives = Math.floor(value / 5);
-  }
-
-  function calculateSingles(value) {
-    calculatedChange.Singles = Math.floor(value / 1);
-  }
-
-  function calculateQuarters(value) {
-    calculatedChange.Quarters = Math.floor(value / 0.25);
-  }
-
-  function calculateDimes(value) {
-    calculatedChange.Dimes = Math.floor(value / 0.1);
-  }
-
-  function calculateNickels(value) {
-    calculatedChange.Nickels = Math.floor(value / 0.05);
-  }
-
-  function calculatePennies(value) {
-    calculatedChange.Pennies = Math.floor(value / 0.01);
-  }
-
   if (value >= 20) {
-    calculateTwenties(value);
+    calculatedChange.Twenties = Math.floor(value / 20);
     value -= calculatedChange.Twenties * 20;
     value = value.toFixed(2);
   }
 
   if (value >= 10) {
-    calculateTens(value);
+    calculatedChange.Tens = Math.floor(value / 10);
     value -= calculatedChange.Tens * 10;
     value = value.toFixed(2);
   }
 
   if (value >= 5) {
-    calculateFives(value);
+    calculatedChange.Fives = Math.floor(value / 5);
     value -= calculatedChange.Fives * 5;
     value = value.toFixed(2);
   }
 
   if (value >= 1) {
-    calculateSingles(value);
+    calculatedChange.Singles = Math.floor(value / 1);
     value -= calculatedChange.Singles * 1;
     value = value.toFixed(2);
   }
 
   if (value >= 0.25) {
-    calculateQuarters(value);
+    calculatedChange.Quarters = Math.floor(value / 0.25);
     value -= calculatedChange.Quarters * 0.25;
     value = value.toFixed(2);
   }
 
   if (value >= 0.1) {
-    calculateDimes(value);
+    calculatedChange.Dimes = Math.floor(value / 0.1);
     value -= calculatedChange.Dimes * 0.1;
     value = value.toFixed(2);
   }
 
   if (value >= 0.05) {
-    calculateNickels(value);
+    calculatedChange.Nickels = Math.floor(value / 0.05);
     value -= calculatedChange.Nickels * 0.05;
     value = value.toFixed(2);
   }
   if (value > 0) {
-    calculatePennies(value);
+    calculatedChange.Pennies = Math.floor(value / 0.01);
     value -= calculatedChange.Pennies * 0.01;
   }
   return calculatedChange;
